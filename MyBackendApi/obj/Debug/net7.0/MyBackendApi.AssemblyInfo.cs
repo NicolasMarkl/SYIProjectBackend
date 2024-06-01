5f846854d49a10f210917c8dedd7fdc3b376b86b
Nicolas Markl <nicolas.markl@apro.at>
2024-06-01 12:35:13 +0200

    add git ignore

--- MyBackendApi/obj/Debug/net7.0/MyBackendApi.AssemblyInfo.cs
+++ MyBackendApi/obj/Debug/net7.0/MyBackendApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBackendApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7016faa87212b0d5661acdabb48d5508d67ac59c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a69109701fbd69484778fd0e43a67119dea1e70")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBackendApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBackendApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
